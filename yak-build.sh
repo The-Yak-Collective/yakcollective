2cbcf2454ed1fda50b857b94ffe102f06279abd3
Nathan Acks <nathan.acks@ecopunk.info>
2020-05-24 11:58:42 -0600

    Move towards formally tracking member posts in GitHub

--- yak-build.sh
+++ yak-build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 bundle install
-./yak-make-ini.sh > yak-planet.ini
-bundle exec pluto update yak-planet.ini
-./yak-make-posts.rb
-find ./writings/_posts -type f -iname '*.html' -exec sed -i -e 's/{/\&#x007B;/g;s/}/\&#x007D;/g;s/%/\&#x0023;/g' "{}" \;
+#./yak-make-ini.sh > yak-planet.ini
+#bundle exec pluto update yak-planet.ini
+#./yak-make-posts.rb
+#find ./writings/_posts -type f -iname '*.html' -exec sed -i -e 's/{/\&#x007B;/g;s/}/\&#x007D;/g;s/%/\&#x0023;/g' "{}" \;
 
 if [[ "$1" == "serve" ]]; then
 	bundle exec jekyll serve
