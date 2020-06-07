43c7fcab9514d0a3440215d62dea9bdd5b7f8a6b
Nathan Acks <nathan.acks@ecopunk.info>
2020-06-06 18:42:46 -0600

    Something a little more complex

--- functions/create-post.js
+++ functions/create-post.js
@@ -1,10 +1,44 @@
-const faker = require('faker')
+// Load required packages
+//
+const express = require("express");
+const bodyParser = require("body-parser");
+const moment = require("moment");
+const chrono = require("chrono-node");
+const accents = require('remove-accents');
+const { Octokit } = require("@octokit/rest");
 
-exports.handler = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: `Hi ${faker.name.findName()}`,
-    })
-  }
+// Lambda function handler
+//
+exports.handler = function(event, context, callback) {
+
+	// Init GitHub connection
+	//
+	//const github = new Octokit({
+	//	auth: process.env.GH_TOKEN
+	//});
+
+	// Reject requests without the right secret
+	//
+	if (event.token !== process.env.WEBHOOK_TOKEN) {
+		return callback(null, {
+			statusCode: 401,
+			body: "Access denied"
+		})
+	}
+
+	// Set incoming post category, or none
+	//
+	var postPath;
+	if (event.category === null || event.category.length === 0) {
+		postPath = "_posts"
+	} else {
+		postPath = event.category + "/_posts"
+	}
+
+	// Demo return
+	//
+	return callback(null, {
+		statusCode: 200,
+		body: "Eventually, I will push posts to " + process.env.GH_USER_OR_TEAM + "/" + process.env.GH_REPO + ":" + process.env.GH_BRANCH + "/" + postPath
+	})
 }
