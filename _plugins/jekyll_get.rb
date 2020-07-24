fa796a70d61cbf191f07b8d9ee3ad97b124f0490
tilnow <44685516+tilnow@users.noreply.github.com>
2020-07-24 11:37:11 +0300

    Update jekyll_get.rb

--- _plugins/jekyll_get.rb
+++ _plugins/jekyll_get.rb
@@ -27,32 +27,33 @@ module Jekyll_Get
 
             
 #          end
-            numissues=source[0]['number'].to_i
-            p "it has issues"
-            p (d['data'] == "issues")
-            p numissues
-            for i in 55..numissues #fundge as going 1-63 gets stuck on 30 due to rate limit
-                p i
-                tmp=JSON.load(open(d['json']+"/"+i.to_s+'/reactions',
-"Accept" =>"application/vnd.github.squirrel-girl-preview+json"))
-                p "read it"
-                #puts JSON.pretty_generate(tmp)
-                #p tmp
-                p i
-                puts "now the big one"
-                source[numissues-i]['reactions']= tmp #reverse order because issues provided in reverse order by github
-                puts "survived"
+            if (d['data'] == "issues")
+                numissues=source[0]['number'].to_i
+                p "it has issues"
+                p numissues
+                for i in 55..numissues #fundge as going 1-63 gets stuck on 30 due to rate limit
+                    p i
+                    tmp=JSON.load(open(d['json']+"/"+i.to_s+'/reactions',
+    "Accept" =>"application/vnd.github.squirrel-girl-preview+json"))
+                    p "read it"
+                    #puts JSON.pretty_generate(tmp)
+                    #p tmp
+                    p i
+                    puts "now the big one"
+                    source[numissues-i]['reactions']= tmp #reverse order because issues provided in reverse order by github
+                    puts "survived"
+                end
+                site.data[d['data']] = source #is a readonly after set!?
+                puts JSON.pretty_generate(site.data['issues'][0])
+              #end
+                      if d['cache']
+                        data_source = (site.config['data_source'] || '_data')
+                        path = "#{data_source}/#{d['data']}.json"
+                        open(path, 'wb') do |file|
+                          file << JSON.generate(site.data[d['data']])
+                        end
+                      end
             end
-            site.data[d['data']] = source #is a readonly after set!?
-            p JSON.pretty_generate(site.data['issues'][0])
-          #end
-          if d['cache']
-            data_source = (site.config['data_source'] || '_data')
-            path = "#{data_source}/#{d['data']}.json"
-            open(path, 'wb') do |file|
-              file << JSON.generate(site.data[d['data']])
-            end
-          end
         rescue
           next
         end
