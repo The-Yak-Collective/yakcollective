4c72268da089bf988736d4beb0d0b51f81bb898b
tilnow <44685516+tilnow@users.noreply.github.com>
2020-07-24 11:01:10 +0300

    Update jekyll_get.rb

--- _plugins/jekyll_get.rb
+++ _plugins/jekyll_get.rb
@@ -23,46 +23,27 @@ module Jekyll_Get
 #          if target #give up option of overloading as we have trouble getting the hash-joiner in
 #            HashJoiner.deep_merge target, source
 #          else
-            source[0]['reactions']=17
-            site.data[d['data']] = source
+
+
             
 #          end
-          p d['data']
-          puts d['data']
-          puts "random string"
-          puts JSON.pretty_generate(site.data['issues'][0])
-          puts "another random string"
-          p site.data['issues'][0]['number']
-          #if d['data'] == "issues" #it always is, no?
-            numissues=site.data['issues'][0]['number'].to_i
-            p "it was issues"
+            numissues=source[0]['number'].to_i
+            p "it has issues"
             p numissues
             for i in 55..numissues #fundge as going 1-63 gets stuck on 30 due to rate limit
                 p i
-                p i.to_s
                 tmp=JSON.load(open('https://api.github.com/repos/cranec-project/Covid-19/issues/'+i.to_s+'/reactions',
 "Accept" =>"application/vnd.github.squirrel-girl-preview+json"))
                 p "read it"
                 puts JSON.pretty_generate(tmp)
                 p tmp
                 p i
-                p site.data['issues'][i]
-                puts "and without i?"
-                p "works so no need to show it" #JSON.pretty_generate(site.data['issues'])
-                puts "let try number again"
-                p site.data['issues'][0]['number']
-                puts "can we see 63"
-                p site.data['issues'][63]
-                puts "can we see '63'"
-                #puts site.data['issues']['63'] #does not work 
-                p "can we see a number"
-                # puts site.data['issues'][i]['number'] #crashes
                 puts "now the big one"
-                site.data['issues'][i] << tmp #tmp #also crashes. probbaly due to teh i index. doe sit crash when existing? yes. now trying to set ne wobject altogether
+                source[numissues-i]['reactions']= tmp #reverse order because issues provided in reverse order by github
                 puts "survived"
-                p "reactions="
-                #p site.data['issues'][i]['reactions']
             end
+            site.data[d['data']] = source #is a readonly after set!?
+            p site.data['issues'][0]['reactions'] 
           #end
           if d['cache']
             data_source = (site.config['data_source'] || '_data')
