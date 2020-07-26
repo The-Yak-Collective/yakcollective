598a0c25b70f704c33146390e62cb7f5bda197d2
tilnow <44685516+tilnow@users.noreply.github.com>
2020-07-26 19:43:16 +0300

    Update makefeatured.rb

--- _plugins/makefeatured.rb
+++ _plugins/makefeatured.rb
@@ -1,16 +1,19 @@
-#thank you https://github.com/18F/jekyll-get
-require 'json'
-#require 'hash-joiner'
-require 'open-uri'
 
-module Jekyll_Get
+require 'json'
+
+
+module makefeatured
   class Generator < Jekyll::Generator
     safe true
     priority :highest
 
     def generate(site)
       td=Time.now.to_i %(3600*24)
+      puts "have time"
+      puts td
       site.data['featured']=td
+      puts "set site var"
+      puts site.data['featured']
     end
   end
 end
