f84702baefdb0f27f5b90b9a80266728bfd86ad7
tilnow <44685516+tilnow@users.noreply.github.com>
2020-07-27 23:16:25 +0300

    add link support

--- _plugins/make_roam_pages.rb
+++ _plugins/make_roam_pages.rb
@@ -38,11 +38,8 @@ module Jekyll
       @base = base
       @dir  = dir
 
-      t=pag['title'].dup
-      t.gsub!(' ','_')
-      n=Addressable::URI.encode_component(t)
 
-      @name = n << '.md'
+      @name = ttl2link(pag['title']) << '.md'
 
       begin 
           self.process(@name)
@@ -57,9 +54,17 @@ module Jekyll
       end
     end
 #json records have title, always. some have "children". can have both string and children.  each children has a string, i think. children can have children. have []() links and [[]]links
+      def ttl2link(s)
+        t=s.dup
+        t.gsub!(' ','_')
+        return Addressable::URI.encode_component(t)
+      end
+
       def render(obj,n)
         obj.each_key do |k|
             if k == 'string'
+                s=obj[k]
+                s=linkify(s,0)
                 return  '&nbsp;&nbsp;&nbsp;&nbsp;'*n <<  '* ' << obj[k] << "\n\n"
             else
                 if k == 'children'
@@ -72,6 +77,20 @@ module Jekyll
             end
         end
         return ''
+      end
+      def linkify(s,i0)
+        sp=s.index('[[',i0)
+        ep=s.index(']]',i0)
+        if sp && ep && (sp<ep)
+            
+            s1=s[0..sp+2]
+            s2=s[sp+2..ep]
+            s3=s[ep..-1]
+            lnk=' [' << s2 << '](' << dir << '/' << ttl2link(s2) << ')' 
+            return linkify(s1 << lnk <<s3,ep+2)
+        else
+            return s
+        end
       end
   end
 end
