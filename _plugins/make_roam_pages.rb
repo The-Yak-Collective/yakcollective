b7461e0ee57dd3884c15e338be163fad7378f959
tilnow <44685516+tilnow@users.noreply.github.com>
2020-07-27 18:46:09 +0300

    Update make_roam_pages.rb

--- _plugins/make_roam_pages.rb
+++ _plugins/make_roam_pages.rb
@@ -16,10 +16,11 @@ p 1
       p 2
         dir =  'roam'
         p 3
-        site.data['roam'].each do |page| #page was catagory in original code
+        site.data['roam'].each do |pag| #page was catagory in original code
         p 4
-        p page
-          site.pages << RoamPage.new(site, site.source, dir, page)
+        p pag
+        p pag.title
+          site.pages << RoamPage.new(site, site.source, dir, pag)
         end
        
       end
@@ -32,23 +33,25 @@ p 1
 
   # A Page subclass. page has contenst and roam_url. and title
   class RoamPage < Page
-    def initialize(site, base, dir, page)
+    def initialize(site, base, dir, pag)
     p 5
-    p site,base,dir,page.title
+    p site,base,dir,pag.title
     p 5.1
       @site = site
       @base = base
       @dir  = dir
 p 5.5
-      @name = Addressable::URI.encode_component(page.title)
+      @name = Addressable::URI.encode_component(pag.title)
 p 6
       begin 
           self.process(@name)
           self.read_yaml(File.join(base, '_layouts'), 'roam_format.html')
           self.data['content']="no content for now" #page.string
-          self.data['title'] = page.title
+          self.data['title'] = pag.title
       rescue
-          puts "failed for page builder", page.title
+          puts "failed for page builder"
+          puts  pag.title
+          puts "managed to print title..."
       end
     end
   end
