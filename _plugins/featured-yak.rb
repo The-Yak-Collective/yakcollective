15bf59b4ec3a824ee690367d5f99bc83abcb81ca
Nathan Acks <nathan.acks@ecopunk.info>
2020-09-09 22:16:59 -0600

    The triumphant return of Maier's "featured yak" plugin

---
+++ _plugins/featured-yak.rb
@@ -0,0 +1,53 @@
+# Standard-ish Jekyll generator plugin lead-in. See:
+#
+#     https://jekyllrb.com/docs/plugins/generators/
+#
+# Note that module names must start with a capital letter, because...
+# Reasons.
+#
+module MakeFeaturedYak
+	class Generator < Jekyll::Generator
+		safe true
+
+		def generate(site)
+
+			# What is today's date? We use a `Time` object, since
+			# that's how Jekyll stores timestamps internally.
+			#
+			# Unfortunately, I can't find a sensible way to build a
+			# `Time` object that corresponds to "the start of today",
+			# hence the monstrosity in the first line below.
+			#
+			buildDate = Time.new(Time.now.year, Time.now.month, Time.now.day)
+			site.data["build_date"] = buildDate
+			puts "Featured yak build date: #{buildDate.strftime('%Y-%m-%d')}"
+
+			# Only members whose `date` property is on or after
+			# `buildDate` should be considered.
+			#
+			currentMembers = Array.new
+			site.collections["members"].docs.each do |member|
+				if member.date <= buildDate
+					currentMembers << member
+				end
+			end
+			currentMemberCount = currentMembers.length()
+			puts "Currently active members: #{currentMemberCount}"
+
+			# Grab a pseudo-random current member for our featured yak.
+			# We do this by taking the current date in YYYYMMDD form
+			# modulo the `currentMemberCount`, and then using that as
+			# our array index. The reason to do this, rather than just
+			# using Ruby's rand(), is to ensure that while the featured
+			# yak is random-ish for outside observers, it is actually
+			# deterministic with respect to the build date (since the
+			# featured yak should be the same for every build in a
+			# given day).
+			#
+			featuredYakIndex = buildDate.strftime("%Y%m%d").to_i % currentMemberCount
+			featuredYak = currentMembers[featuredYakIndex]
+			site.data["featured_yak"] = featuredYak
+			puts "Today's featured yak is: #{featuredYak.data['title']}"
+		end
+	end
+end
