1ac1cc9bb2835647d72a00e1a6a8e7e97364ee3e
Nathan Acks <nathan.acks@ecopunk.info>
2020-11-14 16:35:53 -0700

    Fullscreen version of the "Yak Network Map" looks good enough, I guess

--- projects/yak-network-map/js/main.js
+++ projects/yak-network-map/js/main.js
@@ -559,7 +559,7 @@ function nodeActive(a) {
             e.push(h)
         }
 
-        if (image_attribute) {
+        if ("image_attribute" in f.attributes) {
         	//image_index = jQuery.inArray(image_attribute, temp_array);
         	$GP.info_name.html("<div><img src=" + f.attributes[image_attribute] + " style=\"vertical-align:middle\" /> <span onmouseover=\"sigInst._core.plotter.drawHoverNode(sigInst._core.graph.nodesIndex['" + b.id + '\'])" onmouseout="sigInst.refresh()">' + b.label + "</span></div>");
         } else {
