8208b8e47d718d38a7ab6df52087850f7acc5405
Nathan Acks <nathan.acks@cardboard-iguana.com>
2025-01-03 10:43:33 -0700

    Fixed typo

--- .automation/overlay/quartz.config.ts
+++ .automation/overlay/quartz.config.ts
@@ -32,7 +32,7 @@ const config: QuartzConfig = {
           lightgray:     "hsla( 0,   0%,  90%, 100%)", // --ui1
           gray:          "hsla( 0,   0%,  84%, 100%)", // --ui2
           darkgray:      "hsla( 0,   0%,   6%, 100%)", // --tx1
-          dark:          "hsla( 0,   0%,   4%, 100%)", // --tx1 :: calc(var(--accentl) - 92%)
+          dark:          "hsla( 0,   0%,   4%, 100%)", // --tx1 :: calc(var(--base-l) - 92%)
           secondary:     "hsla(37,  71%,  47%, 100%)", // --ax1
           tertiary:      "hsla(37,  71%,  39%, 100%)", // --ax2
           highlight:     "hsla(37,  50%,  76%,  30%)", // --hl1
@@ -43,7 +43,7 @@ const config: QuartzConfig = {
           lightgray:     "hsla( 0,   0%,  21%, 100%)", // --ui1
           gray:          "hsla( 0,   0%,  27%, 100%)", // --ui2
           darkgray:      "hsla( 0,   0%,  82%, 100%)", // --tx1
-          dark:          "hsla( 0,   0%,  86%, 100%)", // --tx1 :: calc(var(--accentl) + 71%)
+          dark:          "hsla( 0,   0%,  86%, 100%)", // --tx1 :: calc(var(--base-l) + 71%)
           secondary:     "hsla(37,  71%,  56%, 100%)", // --ax1 :: calc(var(--accent-l) * 60 / 50)
           tertiary:      "hsla(37,  71%,  64%, 100%)", // --ax2
           highlight:     "hsla(37,  50%,  40%,  30%)", // --hl1
