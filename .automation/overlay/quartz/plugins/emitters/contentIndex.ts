368d87c1cbe13fee8b762ab2b6a03144ccdda9b8
Nathan Acks <nathan.acks@cardboard-iguana.com>
2025-02-20 22:09:14 -0700

    Quartz updates

--- .automation/overlay/quartz/plugins/emitters/contentIndex.ts
+++ .automation/overlay/quartz/plugins/emitters/contentIndex.ts
@@ -85,7 +85,7 @@ function generateRSSFeed(cfg: GlobalConfiguration, idx: ContentIndex, limit?: nu
       <description>The latest from ${escapeHTML(
         cfg.pageTitle,
       )}</description>
-      <generator>Quartz - quartz.jzhao.xyz</generator>
+      <generator>Quartz — quartz.jzhao.xyz</generator>
       ${items}
     </channel>
   </rss>`
