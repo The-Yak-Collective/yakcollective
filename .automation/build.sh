f4f0e65639539236dfba7b308723e5d9305857b6
Nathan Acks <nathan.acks@cardboard-iguana.com>
2025-08-24 10:43:56 -0600

    Adjust indent

--- .automation/build.sh
+++ .automation/build.sh
@@ -86,13 +86,13 @@ fi
 	cd build/quartz
 	if [[ "$1" == "serve" ]]; then
 		npm exec -- quartz build \
-		          --directory ../src \
-		          --output ../../www \
-		          --serve
+		                 --directory ../src \
+		                 --output ../../www \
+		                 --serve
 	else
 		npm exec -- quartz build \
-		          --directory ../src \
-		          --output ../../www
+		                 --directory ../src \
+		                 --output ../../www
 	fi
 	sed -i'' -e 's#href=\&quot;[\./]\+/#href=\&quot;https://cardboard-iguana.com/grimoire/#g;s#src=\&quot;[\./]\+/#src=\&quot;https://cardboard-iguana.com/grimoire/#g;' ../../www/index.xml
 )
